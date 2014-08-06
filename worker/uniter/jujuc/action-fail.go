b7b5896226884cbd0ab8b1c020189657781c08c2
Bodie Solomon <binary132@gmail.com>
2014-08-06 14:24:14 -0400

    Initial work

---
+++ worker/uniter/jujuc/action-fail.go
@@ -0,0 +1,132 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package jujuc
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/juju/cmd"
+	"launchpad.net/gnuflag"
+)
+
+type nestedMap map[string]interface{}
+
+// ActionSetCommand implements the relation-get command.
+type ActionSetCommand struct {
+	cmd.CommandBase
+	ctx  Context
+	args map[string]string
+}
+
+// NewActionSetCommand returns a new ActionSetCommand with the given context.
+func NewActionSetCommand(ctx Context) cmd.Command {
+	return &ActionSetCommand{ctx: ctx}
+}
+
+// Info returns the content for --help.
+func (c *ActionSetCommand) Info() *cmd.Info {
+	doc := `
+action-set adds the given values to the results map of the Action.  This map
+is returned to the user after the completion of the Action.
+
+Example usage:
+ action-set outfile.size=10G
+ action-set foo.bar.baz=2 foo.bar.zab=3
+ action-set foo.bar.baz=4
+
+ will yield:
+
+ outfile:
+   size: "10G"
+ foo:
+   bar:
+     baz: "4"
+     zab: "3"
+`
+	return &cmd.Info{
+		Name:    "action-set",
+		Args:    "<key>=<value> [<key>.<key>....=<value> ...]",
+		Purpose: "set action results",
+		Doc:     doc,
+	}
+}
+
+// SetFlags handles known option flags.
+// TODO(binary132): add cmd.Input type as in cmd.Output for YAML piping.
+func (c *ActionSetCommand) SetFlags(f *gnuflag.FlagSet) {
+}
+
+// Init accepts maps in the form of key=value, key.key2.keyN....=value
+func (c *ActionSetCommand) Init(args []string) error {
+	c.args = make(map[string]string)
+	for _, arg := range args {
+		thisArg := strings.SplitN(arg, "=", 2)
+		if len(thisArg) != 2 {
+			return fmt.Errorf("argument %q must be of the form key...=value", arg)
+		}
+		c.args[thisArg[0]] = thisArg[1]
+	}
+
+	return nil
+}
+
+// Run adds the given phrases (such as foo.bar=baz) to the existing map of
+// results for the Action.
+func (c *ActionSetCommand) Run(ctx *cmd.Context) error {
+	if len(c.args) == 0 {
+		return nil
+	}
+
+	// c.ctx.ActionResults() will get us the current map of
+	// action results, allowing us to add to it.  We don't care if it's
+	// already failed, so the user doesn't lose useful debugging info.
+	results, _ := c.ctx.ActionResults()
+	nm := nestedMap(results)
+
+	for keys, value := range c.args {
+		keySlice := strings.Split(keys, ".")
+		nm.addValueToMap(keySlice, value)
+	}
+
+	c.ctx.ActionSetResults(map[string]interface{}(nm))
+	return nil
+}
+
+// addValueToMap adds the given value to the map on which the method is run.
+// This allows us to merge maps such as {foo: {bar: baz}} and {foo: {baz: faz}}
+// into {foo: {bar: baz, baz: faz}}.
+func (nm nestedMap) addValueToMap(keys []string, value string) {
+	next := nm
+
+	for i := range keys {
+		// if we are on last key set the value.
+		// shouldn't be a problem.  overwrites existing vals.
+		if i == len(keys)-1 {
+			next[keys[i]] = value
+			break
+		}
+
+		if iface, ok := next[keys[i]]; ok {
+			switch typed := iface.(type) {
+			case map[string]interface{}:
+				// If we already had a map inside, keep
+				// stepping through.
+				next = typed
+			default:
+				// If we didn't, then overwrite value
+				// with a map and iterate with that.
+				m := map[string]interface{}{}
+				next[keys[i]] = m
+				next = m
+			}
+		} else {
+			// Otherwise, it wasn't present, so make it and step
+			// into.
+			m := map[string]interface{}{}
+			next[keys[i]] = m
+			next = m
+		}
+	}
+}
---
+++ worker/uniter/jujuc/action-fail_test.go
@@ -0,0 +1,188 @@
+// Copyright 2014 Canonical Ltd.
+// Licensed under the AGPLv3, see LICENCE file for details.
+
+package jujuc_test
+
+import (
+	"github.com/juju/cmd"
+	jc "github.com/juju/testing/checkers"
+	gc "launchpad.net/gocheck"
+
+	"github.com/juju/juju/testing"
+	"github.com/juju/juju/worker/uniter/jujuc"
+)
+
+type ActionSetSuite struct {
+	ContextSuite
+}
+
+var _ = gc.Suite(&ActionSetSuite{})
+
+func (s *ActionSetSuite) TestGoodActionSet(c *gc.C) {
+	var actionSetTests = []struct {
+		summary  string
+		commands [][]string
+		results  map[string]interface{}
+		errMsg   string
+		code     int
+	}{{
+		summary: "bare value(s) are an Init error",
+		commands: [][]string{
+			[]string{"result"},
+		},
+		errMsg: "error: argument \"result\" must be of the form key...=value\n",
+		code:   2,
+	}, {
+		summary: "a response of one key to one value",
+		commands: [][]string{
+			[]string{"outfile=foo.bz2"},
+		},
+		results: map[string]interface{}{
+			"outfile": "foo.bz2",
+		},
+	}, {
+		summary: "two keys, two values",
+		commands: [][]string{
+			[]string{"outfile=foo.bz2", "size=10G"},
+		},
+		results: map[string]interface{}{
+			"outfile": "foo.bz2",
+			"size":    "10G",
+		},
+	}, {
+		summary: "multiple = are ok",
+		commands: [][]string{
+			[]string{"outfile=foo=bz2"},
+		},
+		results: map[string]interface{}{
+			"outfile": "foo=bz2",
+		},
+	}, {
+		summary: "several interleaved values",
+		commands: [][]string{
+			[]string{"outfile.name=foo.bz2",
+				"outfile.kind.util=bzip2",
+				"outfile.kind.ratio=high"},
+		},
+		results: map[string]interface{}{
+			"outfile": map[string]interface{}{
+				"name": "foo.bz2",
+				"kind": map[string]interface{}{
+					"util":  "bzip2",
+					"ratio": "high",
+				},
+			},
+		},
+	}, {
+		summary: "conflicting simple values in one command result in overwrite",
+		commands: [][]string{
+			[]string{"util=bzip2", "util=5"},
+		},
+		results: map[string]interface{}{
+			"util": "5",
+		},
+	}, {
+		summary: "conflicting simple values in two commands results in overwrite",
+		commands: [][]string{
+			[]string{"util=bzip2"},
+			[]string{"util=5"},
+		},
+		results: map[string]interface{}{
+			"util": "5",
+		},
+	}, {
+		summary: "conflicted map spec: {map1:{key:val}} vs {map1:val2}",
+		commands: [][]string{
+			[]string{"map1.key=val", "map1=val"},
+		},
+		results: map[string]interface{}{
+			"map1": "val",
+		},
+	}, {
+		summary: "two-invocation conflicted map spec: {map1:{key:val}} vs {map1:val2}",
+		commands: [][]string{
+			[]string{"map1.key=val"},
+			[]string{"map1=val"},
+		},
+		results: map[string]interface{}{
+			"map1": "val",
+		},
+	}, {
+		summary: "conflicted map spec: {map1:val2} vs {map1:{key:val}}",
+		commands: [][]string{
+			[]string{"map1=val", "map1.key=val"},
+		},
+		results: map[string]interface{}{
+			"map1": map[string]interface{}{
+				"key": "val",
+			},
+		},
+	}, {
+		summary: "two-invocation conflicted map spec: {map1:val2} vs {map1:{key:val}}",
+		commands: [][]string{
+			[]string{"map1=val"},
+			[]string{"map1.key=val"},
+		},
+		results: map[string]interface{}{
+			"map1": map[string]interface{}{
+				"key": "val",
+			},
+		},
+	}}
+
+	for i, t := range actionSetTests {
+		c.Logf("test %d: %s", i, t.summary)
+		hctx := s.GetHookContext(c, -1, "")
+		com, err := jujuc.NewCommand(hctx, "action-set")
+		c.Assert(err, gc.IsNil)
+		ctx := testing.Context(c)
+		for j, command := range t.commands {
+			c.Logf("  command %d: %#v", j, command)
+			code := cmd.Main(com, ctx, command)
+			_, failed := hctx.ActionResults()
+			if t.code == 0 {
+				c.Check(code, gc.Equals, 0)
+				c.Check(bufferString(ctx.Stderr), gc.Equals, "")
+				c.Check(failed, gc.Equals, false)
+				if j == len(t.commands)-1 {
+					results, _ := hctx.ActionResults()
+					c.Check(results, jc.DeepEquals, t.results)
+				}
+			} else {
+				c.Check(bufferString(ctx.Stderr), gc.Equals, t.errMsg)
+				c.Check(code, gc.Equals, t.code)
+				c.Check(failed, gc.Equals, false)
+			}
+		}
+	}
+}
+
+func (s *ActionSetSuite) TestHelp(c *gc.C) {
+	hctx := s.GetHookContext(c, -1, "")
+	com, err := jujuc.NewCommand(hctx, "action-set")
+	c.Assert(err, gc.IsNil)
+	ctx := testing.Context(c)
+	code := cmd.Main(com, ctx, []string{"--help"})
+	c.Assert(code, gc.Equals, 0)
+	c.Assert(bufferString(ctx.Stdout), gc.Equals, `usage: action-set <key>=<value> [<key>.<key>....=<value> ...]
+purpose: set action results
+
+action-set adds the given values to the results map of the Action.  This map
+is returned to the user after the completion of the Action.
+
+Example usage:
+ action-set outfile.size=10G
+ action-set foo.bar.baz=2 foo.bar.zab=3
+ action-set foo.bar.baz=4
+
+ will yield:
+
+ outfile:
+   size: "10G"
+ foo:
+   bar:
+     baz: "4"
+     zab: "3"
+`)
+	c.Assert(bufferString(ctx.Stderr), gc.Equals, "")
+}
