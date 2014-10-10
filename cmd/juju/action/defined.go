dc81acaae1f1cab39f985f37dad700855c9cb5a4
Bodie Solomon <binary132@gmail.com>
2014-10-10 13:49:24 -0400

    WIP action defined subcommand: needs testing

--- cmd/juju/action/defined.go
+++ cmd/juju/action/defined.go
@@ -3,12 +3,18 @@
 
 package action
 
-import "github.com/juju/cmd"
+import (
+	"github.com/juju/cmd"
+	errors "github.com/juju/errors"
+	"github.com/juju/names"
+	"launchpad.net/gnuflag"
+)
 
 // DefinedCommand lists actions defined by the charm of a given service.
 type DefinedCommand struct {
 	ActionCommandBase
-	undefinedActionCommand
+	ServiceTag names.ServiceTag
+	out        cmd.Output
 }
 
 const definedDoc = `
@@ -21,8 +27,50 @@ For more information, see also the 'do' subcommand, which executes actions.
 func (c *DefinedCommand) Info() *cmd.Info {
 	return &cmd.Info{
 		Name:    "defined",
-		Args:    "<service name> [--schema]",
+		Args:    "[--schema] <service name>",
 		Purpose: "WIP: show actions defined for a service",
 		Doc:     definedDoc,
 	}
+}
+
+// Init validates the service name and any other options.
+func (c *DefinedCommand) Init(args []string) error {
+	switch len(args) {
+	case 0:
+		return errors.New("no service name specified")
+	case 1:
+		svcName := args[0]
+		if !names.IsValidService(svcName) {
+			return errors.Errorf("invalid service name %q", svcName)
+		}
+		c.ServiceTag = names.NewServiceTag(svcName)
+		return nil
+	default:
+		return cmd.CheckEmpty(args[1:])
+	}
+}
+
+// Set up the YAML output.
+func (c *DefinedCommand) SetFlags(f *gnuflag.FlagSet) {
+	// TODO(binary132) add json output?
+	c.out.AddFlags(f, "yaml", map[string]cmd.Formatter{
+		"yaml": cmd.FormatYaml,
+	})
+}
+
+// Run grabs the Actions spec from the api.  It then sets up a sensible
+// output format for the map.
+func (c *DefinedCommand) Run(ctx *cmd.Context) error {
+	api, err := c.NewActionAPIClient()
+	if err != nil {
+		return err
+	}
+	defer api.Close()
+
+	actions, err := api.ServiceCharmActions(c.ServiceTag)
+	if err != nil {
+		return err
+	}
+	actionSpecs := actions.ActionSpecs
+	return c.out.Write(ctx, actionSpecs)
 }
