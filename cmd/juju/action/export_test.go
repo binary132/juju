cbf45717d704d687c2b8eca23e66c6b344c6ddbf
Bodie Solomon <binary132@gmail.com>
2014-10-23 15:48:07 -0400

    TMP action do subcommand (untested)

--- cmd/juju/action/export_test.go
+++ cmd/juju/action/export_test.go
@@ -20,3 +20,23 @@ func (c *DefinedCommand) FullSchema() bool {
 func (c *FetchCommand) ActionTag() names.ActionTag {
 	return c.actionTag
 }
+
+func (c *DoCommand) UnitTag() names.UnitTag {
+	return c.unitTag
+}
+
+func (c *DoCommand) ActionName() string {
+	return c.actionName
+}
+
+func (c *DoCommand) ActionParams() map[string]interface{} {
+	return c.actionParams
+}
+
+func (c *DoCommand) ParamsYAMLPath() string {
+	return c.paramsYAML.Path
+}
+
+func (c *DoCommand) IsAsync() bool {
+	return c.async
+}
